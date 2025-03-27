eb73bb16b1ed38a69f2d4034b6573e455d01c6b5
matt765 <mateusz.wyrebek@gmail.com>
2025-03-27 22:46:34 +0100

    Move Jest config inside tests folder

--- src/tests/config/jest.config.js
+++ src/tests/config/jest.config.js
@@ -1,16 +1,21 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jest-environment-jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    setupFilesAfterEnv: ['<rootDir>/src/tests/config/jest.setup.ts'],
     moduleNameMapper: {
         '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     },
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
     transform: {
-        '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { configFile: './jest.babel.config.js' }],
+        '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { configFile: './src/tests/config/jest.babel.config.js' }],
     },
     transformIgnorePatterns: [
         '/node_modules/',
         '\\.pnp\\.[^\\/]+$'
     ],
+    rootDir: '../../../',
+    testMatch: [
+        "<rootDir>/src/tests/hooks/**/*.test.tsx",
+        "<rootDir>/src/tests/hooks/**/*.test.ts"
+    ]
 };
